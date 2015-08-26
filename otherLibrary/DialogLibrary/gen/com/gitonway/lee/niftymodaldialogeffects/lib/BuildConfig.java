72f4e409df139e062a1d309be46f252c9ed213c8
guohuanwen <1179636441@qq.com>
2015-08-26 21:23:22 +0800

    a

---
+++ otherLibrary/DialogLibrary/gen/com/gitonway/lee/niftymodaldialogeffects/lib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gitonway.lee.niftymodaldialogeffects.lib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ otherLibrary/jinDuButtonLibrary/gen/com/dd/circular/progress/button/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dd.circular.progress.button;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
